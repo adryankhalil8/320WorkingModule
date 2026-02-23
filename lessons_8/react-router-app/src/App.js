e9e7bd64661e6272fd65a4099774fe20333a9908
adryankhalil8 <adryan.jefferson@peopleshores.com>
2026-02-23 13:14:18 -0500

    Crypto Lab updated

--- lessons_8/react-router-app/src/App.js
+++ lessons_8/react-router-app/src/App.js
@@ -1,28 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function Header() {
-  return (<h1>Simple React Application</h1>);
-}
-
-function Content() {
-  return (<p>This is my first React Application, wish me luck!</p>);
-}
-
-function Footer() {
-  return (<h1>Created by Me, of course.</h1>);
-}
-
-function App() {
+// Import Route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+import Nav from "./components/Nav";
+export default function App () {
+  // We will use the Route component, path attribute, and element attribute to specify each route.
   return (
-    <>
-      <Header />
-      <Content color="blue" text="This is my first React Application!" />
-      <Content color="red" text="Wish me luck..." />
-      <Content color="green" text="I think I've got it!" />
-      <Footer />
-    </>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/currencies" element={<Currencies/>}/>
+        <Route path="/price" element={<Price/>}/>
+      </Routes>
+    </div>
   );
 }
-
-export default App;
